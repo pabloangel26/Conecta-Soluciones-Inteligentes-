5a98e46f0e4f533c826890ed3598b7aa819bc568
Pablo Angel <pabloangel26@gmail.com>
2024-01-25 20:28:31 -0500

    Actualizacion 2 repositorio js

---
+++ ejercicios/modulo1.js
@@ -0,0 +1,21 @@
+//Realizar el algoritmo que convierta litros en centilitros, decilitros, galones y onzas
+
+let litro = 5;
+
+const LitrosToCentilitros = litro * 100;
+
+console.log('valor de litros a centilitros', LitrosToCentilitros)
+
+const LitrosToDeclitros = litro * 10;
+
+console.log('valor de litros a decilitros', LitrosToDeclitros)
+
+
+const LitrosToGalones = litro * 0.264172;
+
+console.log('valor de litros a galones', LitrosToGalones)
+
+
+const LitrosToOnzas = litro * 33.814;
+
+console.log('valor de litros a onzas', LitrosToOnzas)
